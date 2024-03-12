65e8f4824e340e130bfff121039f3081352781d4
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-03-12 12:01:12 +0000

    new commit

--- d-calage.c
+++ d-calage.c
@@ -7,16 +7,30 @@ char *encrypt(char *str)
     while (str[i] != '\0')
     {
         str[i] += 3;
+	i++;
     }
     
     return str;
 }
 
+char *decrypt(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		str[i] -= 3;
+		i++;
+	}
+	return str;
+}
+
 int main()
 {
-    char string[100]
+    char string[100];
     printf("Entrer votre string : \n");
     scanf("%s", string);
-    printf("%s\n", encrypt(string));
+    printf("======encryptage===========\n%s\n", encrypt(string));
+    printf("======decryptage===========\n%s\n", decrypt(string));
     return 0; 
 }
